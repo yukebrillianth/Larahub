ef2f3a323b52e14448631aba55a4ada383c7aa32
yuke brilliant <yukebrilliant@gmail.com>
2020-07-04 05:17:44 +0700

    Update

--- resources/views/Question/index.blade.php
+++ resources/views/Question/index.blade.php
@@ -50,7 +50,27 @@
                     <form action="/pertanyaan/{{ $question->id }}" style="display: inline" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-sm btn-danger" data-toggle="tooltip" data-placement="top" title="Hapus"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                        <button type="button" data-toggle="modal" data-target="#hapusModal" class="btn btn-sm btn-danger" data-toggle="tooltip" data-placement="top" title="Hapus"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="hapusModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Apakah anda yakin?</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                Apakah anda yakin untuk menghapus pertanyaan ini?
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                <button type="submit" class="btn btn-primary">Yakin</button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
                     </form>
                 </td> 
                 </tr>
